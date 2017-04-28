dc59e388fc2029bd1505e0913edf6f9bba4f413b
Richard Fenix <Richard Fenix>
2017-04-27 21:00:52 -0400

    initial site files

---
+++ OneDrive/documents/GT Coding Bootcamp/PREWORK_RF/Module-10/javascript.js
@@ -0,0 +1,28 @@
+
+//Sample how to begin a javascript file from https://www.w3schools.com/js/myscript.js
+//function myFunction() {
+//    document.getElementById("demo").innerHTML="Paragraph changed.";
+//}
+        
+
+function myFunction1() {
+    document.getElementById("box").style.height = "250px";
+}
+
+function myFunction2() {
+    document.getElementById("box").style.background = "blue";
+    document.getElementById("box").style.opacity = "1.0";
+
+}
+
+function myFunction3() {
+    document.getElementById("box").style.opacity = "0.5";
+
+}
+
+
+function myFunction4() {
+    document.getElementById("box").style.height = "150px", document.getElementById("box").style.background = "orange", 
+    document.getElementById("box").style.opacity = "1.0";
+
+};
